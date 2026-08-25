9214ebfbab6c15ceb9d864c91ae572cdfc23933a
herrphil <phillip.j.wray.esq@gmail.com>
2026-08-25 12:04:33 -0600

    Add bulletin repository unit tests.

--- core/data/src/test/java/com/reference/implementation/data/BulletinCacheRepositoryImplTest.kt
+++ core/data/src/test/java/com/reference/implementation/data/BulletinCacheRepositoryImplTest.kt
@@ -119,6 +119,7 @@ class BulletinCacheRepositoryImplTest {
                 assertEquals(NetworkResult.Loading, awaitItem())
 
                 // 3. Launch refreshBulletins() asynchronously in the test scope
+                @Suppress
                 val refreshJob = backgroundScope.launch(testDispatcher) {
                     repository.refreshBulletins(onRetry = { attempt ->
                         retryCount = attempt
